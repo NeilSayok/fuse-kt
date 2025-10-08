ec32b7ee27d9a9fc9fe76756bded0ccc94690cc7
NeilSayok <sayokdeymajumder1998@gmail.com>
2025-10-08 23:43:46 +0530

    Refactor build.gradle.kts to simplify publishing configuration and update repository settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,6 @@ plugins {
     kotlin("multiplatform") version "2.2.0"
     kotlin("plugin.serialization") version "2.2.0"
     `maven-publish`
-//    signing
-    id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
 }
 
 group = "io.github.neilsayok"
@@ -81,67 +79,18 @@ kotlin {
 
 publishing {
     publications.withType<MavenPublication> {
-        pom {
-            name.set("Fuse-kt")
-            description.set("A lightweight fuzzy-search library for Kotlin multiplatform, ported from Fuse.js")
-            url.set("https://github.com/neilsayok/fuse-kt")
-            
-            licenses {
-                license {
-                    name.set("Apache License 2.0")
-                    url.set("https://www.apache.org/licenses/LICENSE-2.0")
-                }
-            }
-            
-            developers {
-                developer {
-                    id.set("neilsayok")
-                    name.set("Sayok Dey Majumder")
-                    email.set("sayokdeymajumder1998@gmail.com")
-                }
-            }
-            
-            scm {
-                connection.set("scm:git:git://github.com/neilsayok/fuse-kt.git")
-                developerConnection.set("scm:git:ssh://github.com:neilsayok/fuse-kt.git")
-                url.set("https://github.com/neilsayok/fuse-kt")
-            }
-        }
+        groupId = "io.github.neilsayok"
+        version = project.version.toString()
     }
-    
+
     repositories {
         maven {
-            name = "sonatype"
-            val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
-            val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
-            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
-            
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/NeilSayok/fuse-kt")
             credentials {
-                username = findProperty("ossrhUsername") as String? ?: System.getenv("OSSRH_USERNAME")
-                password = findProperty("ossrhPassword") as String? ?: System.getenv("OSSRH_PASSWORD")
+                username = providers.gradleProperty("gpr.user").orElse(providers.environmentVariable("USERNAME")).orNull
+                password = providers.gradleProperty("gpr.key").orElse(providers.environmentVariable("TOKEN")).orNull
             }
-        }
-    }
-}
-
-//signing {
-//    val signingKey = findProperty("signingKey") as String? ?: System.getenv("SIGNING_KEY")
-//    val signingPassword = findProperty("signingPassword") as String? ?: System.getenv("SIGNING_PASSWORD")
-//
-//    if (signingKey != null && signingPassword != null) {
-//        useInMemoryPgpKeys(signingKey, signingPassword)
-//        sign(publishing.publications)
-//    }
-//}
-
-nexusPublishing {
-    repositories {
-        sonatype {
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-            
-            username.set(findProperty("ossrhUsername") as String? ?: System.getenv("OSSRH_USERNAME"))
-            password.set(findProperty("ossrhPassword") as String? ?: System.getenv("OSSRH_PASSWORD"))
         }
     }
 }
